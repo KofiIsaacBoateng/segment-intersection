a0f2b0b18d623e57322de8ee297a5e25127f1625
KofiIsaacBoateng <kofiisaacboateng@gmail.com>
2024-01-17 10:38:12 +0000

    update: added coordinate indicators to each edge of a line

---
+++ util.js
@@ -0,0 +1,15 @@
+const drawDot = (ctx, {x, y}, label) => {
+    ctx.beginPath()
+    ctx.strokeStyle = "#fff"
+    ctx.fillStyle = "#222"
+
+    ctx.arc(x, y, 10,0, Math.PI * 2)
+    ctx.fill()
+    ctx.stroke()
+    
+    ctx.fillStyle="#fff"
+    ctx.textAlign = "center"
+    ctx.textBaseline = "middle"
+    ctx.font = "bold 12px Arial"
+    ctx.fillText(label, x, y)
+}
